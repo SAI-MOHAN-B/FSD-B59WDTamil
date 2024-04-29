2eb3aa552f7c310c112eda84b8f17f36072d3a08
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-04-29 20:18:08 +0530

    Update ES5 vs ES6.js

--- ES5 vs ES6.js
+++ ES5 vs ES6.js
@@ -40,3 +40,16 @@ console.log("Fifteen is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
 console.log(`Fifteen is ${a + b} and
 not ${2 * a + b}.`);
 console.log(``);
+
+
+// 1.Write a JavaScript function to get the first element of an array.
+// Passing a parameter 'n' will return the first 'n' elements of the array.
+
+function foo(arr,n){
+if(n == undefined)
+  return arr[0];
+if(n<0)
+  return [];
+return arr.slice(0,n);  
+}
+console.log(foo([7,0,2,5]));
