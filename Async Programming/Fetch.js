3b64e4fbf865597486e609c4b4743242e2e6ff98
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-05-21 21:08:34 +0530

    Update Fetch.js

--- Async Programming/Fetch.js
+++ Async Programming/Fetch.js
@@ -57,3 +57,36 @@ var res =  fetch("https://664c9fc235bbda1098812cad.mockapi.io/data/Users",{
 res.then((data)=>{
     return data.json()
 }).then((data1)=>console.log(data1)).catch((error)=>console.log(error))
+
+
+// Rest Countries
+var res = fetch("https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
+res.then((data) => {
+    return data.json()
+}).then((data1) => foo(data1)).catch((error) => console.log(error))
+
+var container = document.createElement("div");
+container.className = "container";
+
+var row = document.createElement("div");
+row.className = "row";
+
+
+function foo(data1) {
+    console.log(data1);
+    for (var i = 0; i < data1.length; i++) {
+        var col = document.createElement("div");
+        col.className = "col-lg-4";
+        col.innerHTML = `<div class="card border-primary mb-3" style="max-width: 18rem;">
+    <div class="card-header">Rest Countries Data</div>
+    <div class="card-body text-primary">
+      <h5 class="card-title">${data1[i].name}</h5>
+      <p class="card-text">Capital:${data1[i].capital}</p>
+      <p class="card-text">Region:${data1[i].region}</p>
+    </div>`;
+    row.append(col);
+    container.append(row);
+    document.body.append(container);
+    }
+    
+}
